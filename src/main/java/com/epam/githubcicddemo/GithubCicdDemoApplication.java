6256211e263f10a017dc81517187b83bfc07a4c2
Muzaffar_Umarjonov <Muzaffar_Umarjonov@epam.com>
2023-03-22 18:43:03 +0500

    maven yaml and docker config

--- src/main/java/com/epam/githubcicddemo/GithubCicdDemoApplication.java
+++ src/main/java/com/epam/githubcicddemo/GithubCicdDemoApplication.java
@@ -1,6 +1,5 @@
 package com.epam.githubcicddemo;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
